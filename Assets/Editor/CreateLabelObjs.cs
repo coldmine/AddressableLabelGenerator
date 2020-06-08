419136aec58117d0941d4ec8a1314b53508ec061
nergulnergul <yuha8486@gmail.com>
2020-06-08 14:03:52 +0900

    update comment

--- Assets/Editor/CreateLabelObjs.cs
+++ Assets/Editor/CreateLabelObjs.cs
@@ -7,7 +7,9 @@
  *      3. assign labels to your addressable assets
  *      4. Hit Deckard Utils / Get All Labels in menu bar
  *      5. then you can see the AutoGenLabels.cs in Scripts/AllLabels folder
- *      6. it is private static variables, however, you can access in anywhere with AutoGenLabels.GetLabels() to get all labels (variable type = List<String>).
+ *      6. it is private static variables,
+ *         6-1. however, you can access in anywhere with AutoGenLabels.GetLabels() to get all labels 
+ *         6-2. return type is List<String>
  *      7. It should be handy, if you make a downloader.
  * Worked in : 
  *      1. Unity 2019.3.0f
